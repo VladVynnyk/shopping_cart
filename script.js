4ecc830810f9b7ea32d037a7f5cac40a456185cf
Vlad Vynnyk <vladvynnyk1@gmail.com>
2022-11-19 08:09:06 +0200

    changed products

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let products = [{name:"Пралка 1", price:4000},{name:"SONY", price:5000}, {name:"Philips", price:7400}, {name:"Samsung", price:10200}]
+let products = [{name:"Пралка 1", price:4000, img:".\img\18115433.jpg"},{name:"SONY", price:5000, img:".\img\276840966.jpg"}, {name:"Philips", price:7400, img:".\img\УТ000017292-1.jpg"}, {name:"Samsung", price:10200, img:".\img\img_0_139_3518_0_Small.jpg"}]
 let cartProducts = []
 if(document.title == 'Document'){
     products.map((product)=>{
         document.write(`<div class="box">
                         <div class="top_block">
-                            <p><a href="#">ХУДОЖНЯ ЛІТЕРАТУРА</a></p>
+                            <p><a href="#">Пральна машинка</a></p>
                         </div>
                         <div class="midll_block">
-                            <img src="img/169302548.jpg">
-                            <a href="https://rozetka.com.ua/ua/287646223/p287646223/">
+                            <img src="./img/18115433.jpg">
+                            <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.foxtrot.com.ua%2Fuk%2Fshop%2Fstiralki.html&psig=AOvVaw2WXy93H8XbYUxYVUgiHRpV&ust=1668923971471000&source=images&cd=vfe&ved=0CBEQjhxqFwoTCIiX_7XIufsCFQAAAAAdAAAAABAd">
                                 <p>${product.name}</p>
                             </a>
                         </div>
